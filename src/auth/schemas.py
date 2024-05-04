d91506ba5576d627d411a6e0f579b4cf2ec82981
UzNik <uznik.99@mail.ru>
2024-05-04 13:01:53 +0300

    pre redis integration

---
+++ src/auth/schemas.py
@@ -0,0 +1,27 @@
+from fastapi_users import schemas
+
+
+class UserRead(schemas.BaseUser[int]):
+    id: int
+    username: str
+    email: str
+    role_id: int
+    is_active: bool | None = True
+    is_superuser: bool | None = False
+    is_verified: bool | None = False
+
+    class Config:
+        from_attributes = True
+
+
+class UserCreate(schemas.BaseUserCreate):
+    username: str
+    email: str
+    password: str
+    role_id: int
+    is_active: bool | None = True
+    is_superuser: bool | None = False
+    is_verified: bool | None = False
+
+
+    
